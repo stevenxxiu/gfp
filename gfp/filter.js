1b74d448453b3250f27dd079a1cb15440209a95e
Steven Xu <stevenxxiu@gmail.com>
2015-02-11 14:09:35 +1100

    re-implemented filter storage using json, cf #11

--- gfp/filter.js
+++ gfp/filter.js
@@ -1,4 +1,17 @@
-import {ActiveFilter, InvalidFilter, RegExpFilter} from 'gfp/lib/filterClasses';
+import {ActiveFilter, Filter, InvalidFilter, RegExpFilter} from 'gfp/lib/filterClasses';
+
+Filter.prototype.toObject = function(){
+  let res = Object.create(null);
+  if(this instanceof ActiveFilter){
+    if(this._disabled)
+      res.disabled = true;
+    if(this._hitCount)
+      res.hitCount = this._hitCount;
+    if(this._lastHit)
+      res.lastHit = this._lastHit;
+  }
+  return res;
+};
 
 class RegExpFilter extends RegExpFilter {
   constructor(text, regexpSource, matchCase, collapse){
@@ -20,14 +33,17 @@ class RegExpFilter extends RegExpFilter {
     return this.regexp.test(data);
   }
 
-  static fromParts(text, options){
+  static fromParts(text, optionsStr){
+    if(optionsStr)
+      text += `$${optionsStr}`;
     let matchCase = false;
     let collapse = true;
-    options = options.toUpperCase().split(',');
+    let options = optionsStr.toUpperCase().split(',');
     for(let option of options){
       switch(option){
         case 'MATCH_CASE': matchCase = true; break;
         case 'COLLAPSE': collapse = true; break;
+        case '': break;
         default:
           return new InvalidFilter(`Unknown option: ${option}`);
       }
@@ -45,6 +61,13 @@ export class MultiRegExpFilter extends ActiveFilter {
         filter.parent = this;
     }
     this.filters = filters;
+  }
+
+  get text(){
+    let parts = [];
+    for(let filter of this.filters)
+      parts.push(filter.text);
+    return parts.join('$');
   }
 
   get collapse(){
@@ -78,4 +101,8 @@ export class MultiRegExpFilter extends ActiveFilter {
 }
 
 export class BlockingFilter extends MultiRegExpFilter {}
-export class WhitelistFilter extends MultiRegExpFilter {}
+export class WhitelistFilter extends MultiRegExpFilter {
+  get text(){
+    return '@@' + super.text;
+  }
+}
