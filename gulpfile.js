b10d95b4f15472237bd264c5047a6b34771d9194
Steven Xu <stevenxxiu@gmail.com>
2015-02-16 22:27:58 +1100

    switched to karma, cf #13

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,26 @@
+/*jshint -W079*/
 var to5ify = require('6to5ify');
 var browserify = require('browserify');
+var istanbul = require('browserify-istanbul');
 var FirefoxProfile = require('firefox-profile');
-var glob = require('glob');
 var gulp = require('gulp');
 var addsrc = require('gulp-add-src');
 var concat = require('gulp-concat-util');
-var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
 var isparta = require('isparta');
+var karma = require('karma');
 var merge = require('merge');
-var istanbul = require('browserify-istanbul');
+var open = require('open');
 var buffer = require('vinyl-buffer');
 var source = require('vinyl-source-stream');
-
-function bundle(config){
-  config = config || {};
-  config = merge({paths: ['.', './gfp/lib'], transform: [to5ify.configure({blacklist: ['regenerator']})]}, config);
-  // transform run twice bug workaround
-  var transforms = config.transform;
-  delete config.transform;
-  var res = browserify(config);
-  for(var i = 0; i < transforms.length; i++)
-    res.transform(transforms[i]);
-  return res.bundle().on('error', function(err){console.log(err.message);});
-}
 
 gulp.task('build', function(){
   function cb(){
     var fileName = 'google_search_filter_plus.user.js';
-    bundle({entries: 'gfp/main.js'})
+    browserify({paths: ['.', './gfp/lib'], entries: 'gfp/main.js'})
+      .transform(to5ify.configure({blacklist: ['regenerator']}))
+      .on('error', function(err){console.log(err.message);})
+      .bundle()
       .pipe(source(fileName))
       .pipe(buffer())
       .pipe(addsrc('gfp/header.js'))
@@ -38,36 +30,50 @@ gulp.task('build', function(){
   watch('gfp/**/!(test_*.js)', {}, cb); cb();
 });
 
-gulp.task('test', function(){
-  function cb(){
-    var fileName = 'google_search_filter_plus.test.js';
-    bundle({debug: true, entries: glob.sync('gfp/**/test_*.js')})
-      .pipe(source(fileName))
-      .pipe(buffer())
-      .pipe(sourcemaps.init({loadMaps: true}))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('dist'));
-  }
-  watch('gfp/**/*.js', {}, cb); cb();
-});
-
-gulp.task('cover', function(){
-  function cb(){
-    var fileName = 'google_search_filter_plus.cover.js';
-    bundle({
-      entries: glob.sync('gfp/**/test_*.js'),
-      transform: [istanbul({instrumenter: isparta, defaultIgnore: false})]
-    })
-      .pipe(source(fileName))
-      .pipe(gulp.dest('dist'));
-  }
-  watch('gfp/**/*.js', {}, cb); cb();
-});
-
 gulp.task('greasemonkey', ['build'], function(){
   new FirefoxProfile.Finder().getPath('default', function(err, path){
     gulp.src('dist/google_search_filter_plus.user.js').pipe(gulp.dest(path + '/gm_scripts/Google_Search_Filter_Plus'));
   });
 });
 
-gulp.task('default', ['test']);
+var karmaConfig = {
+  common: {
+    frameworks: ['browserify', 'qunit'],
+    files: ['gfp/**/test_*.js'],
+    preprocessors: {
+      'gfp/**/test_*.js': ['browserify'],
+    },
+    browserify: {
+      paths: ['.', './gfp/lib']
+    }
+  },
+  test: {
+    port: 9876,
+    reporters: ['progress'],
+    browserify: {
+      debug: true,
+      transform: [to5ify.configure({blacklist: ['regenerator']})]
+    }
+  },
+  cover: {
+    port: 9877,
+    reporters: ['coverage'],
+    browserify: {
+      transform: [istanbul({instrumenter: isparta, defaultIgnore: false})]
+    }
+  }
+};
+
+gulp.task('test', function(){
+  var config = merge.recursive(karmaConfig.common, karmaConfig.test);
+  karma.server.start(config);
+  open('http://localhost:' + config.port);
+});
+
+gulp.task('cover', function(){
+  var config = merge.recursive(karmaConfig.common, karmaConfig.cover);
+  karma.server.start(config);
+  open('http://localhost:' + config.port);
+});
+
+gulp.task('default', ['test', 'cover']);
