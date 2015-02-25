411daa9c7659d98ce35345ed5e2f8f34652ff5ed
Steven Xu <stevenxxiu@gmail.com>
2015-02-25 22:52:34 +1100

    use firefox's profiler instead

--- gfp/logger.js
+++ gfp/logger.js
@@ -1,45 +1,30 @@
 import Config from 'gfp/config';
 
 export let Logger = {
-	msg(msg){
-		console.log(msg);
-	},
+  msg(msg){
+    console.log(msg);
+  },
 
-	error(msg){
-		console.log(`Error: ${msg}`);
-	}
+  error(msg){
+    console.log(`Error: ${msg}`);
+  }
 };
 
 export let LogTime = {
-	curTime: null,
+  curTime: null,
 
-	start(){
-		LogTime.curTime = new Date().getTime();
-	},
+  start(){
+    this.curTime = new Date().getTime();
+  },
 
-	snap(msg){
-		console.log(`${msg}: ${new Date().getTime() - LogTime.curTime}ms`);
-	},
-
-	restart(msg){
-		LogTime.snap(msg);
-		LogTime.start();
-	},
-
-	profile(obj, funcName){
-		let func = obj[funcName];
-		obj[funcName] = () => {
-			this.start();
-			let res = func.apply(this, arguments);
-			this.snap(funcName);
-			return res;
-		};
-	}
+  snap(msg){
+    console.log(`${msg}: ${new Date().getTime() - this.curTime}ms`);
+  }
 };
 
 if(!Config.logTime){
-	for(let key of Object.keys(LogTime)){
-		if(LogTime[key].constructor == Function)
-			LogTime[key] = () => null;
-	}
+  for(let key of Object.keys(LogTime)){
+    if(LogTime[key].constructor == Function)
+      LogTime[key] = () => null;
+  }
 }
