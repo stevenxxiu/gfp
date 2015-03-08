1d070e29dfc9c5657488edb55f57b8262d8b8bf8
Steven Xu <stevenxxiu@gmail.com>
2015-03-08 23:14:54 +1100

    jqgrid implementation

--- gfp/pref.js
+++ gfp/pref.js
@@ -2,6 +2,8 @@
 import config from 'gfp/config';
 import prefStyle from 'gfp/css/pref.css';
 import prefHTML from 'gfp/html/pref.html';
+import {CombinedMultiMatcher} from 'gfp/matcher';
+import {pad} from 'gfp/utils';
 
 class Pref {
   constructor(){
@@ -12,7 +14,7 @@ class Pref {
         this.addResources();
       if(dialog)
         return;
-      dialog = new PrefDialog().on('dialogclose', () => dialog = null);
+      dialog = new PrefDialog().dialog.on('dialogclose', () => dialog = null);
     }, null);
   }
 
@@ -21,12 +23,14 @@ class Pref {
     GM_addStyle(GM_getResourceText('jquery-ui-css').replace(
       /url\("([^":]+)"\)/g, (match, url) => `url("${GM_getResourceURL('jquery-ui-css/' + url)}")`
     ));
+    GM_addStyle(GM_getResourceText('jqgrid-css'));
   }
 }
 
 class PrefDialog {
   constructor(){
     this.dialog = $(prefHTML).dialog(Object.assign({title: 'Google Search Filter +'}, this.dialogConfig));
+    this.grid = this.dialog.find('.grid');
     this.bindImport();
     this.bindExport();
     this.addGrid();
@@ -41,7 +45,7 @@ class PrefDialog {
   }
 
   bindImport(){
-    $('#gfp-import').click((e) => {
+    this.dialog.find('.import').click((e) => {
       $('<textarea></textarea>')
         .dialog(Object.assign({
           title: 'Import',
@@ -62,7 +66,7 @@ class PrefDialog {
   }
 
   bindExport(){
-    $('#gfp-export').click((e) => {
+    this.dialog.find('.export').click((e) => {
       $('<textarea></textarea>')
         .attr('readonly', 'readonly')
         .val(JSON.stringify(config.filtersObject, null, 2))
@@ -76,11 +80,51 @@ class PrefDialog {
   }
 
   addGrid(){
-
-  }
-
-  on(){
-    return this.dialog.on.apply(this.dialog, arguments);
+    let data = [];
+    for(let filter of config.filters){
+      data.push({
+        id: filter.text,
+        text: filter.text,
+        slow: CombinedMultiMatcher.isSlowFilter(filter),
+        enabled: !filter.disabled,
+        hitCount: filter.hitCount,
+        lastHit: filter.lastHit,
+      });
+    }
+    this.grid.jqGrid({
+      colNames: ['Filter rule', '!', 'Enabled', 'Hits', 'Last hit'],
+      colModel: [
+        {name: 'text', width: 10, editable: true},
+        {name: 'slow', width: 1, align: 'right'},
+        {name: 'enabled', width: 1, align: 'right'},
+        {name: 'hitCount', width: 1, align: 'right'},
+        {name: 'lastHit', width: 3, align: 'right', formatter: (value, options, rowObject) => {
+          let date = new Date(value);
+          return rowObject.hitCount > 0 ? (
+            `${date.getFullYear()}-${pad(date.getMonth() + 1, 2)}-${pad(date.getDate(), 2)} ` +
+            `${pad(date.getHours() + 1, 2)}:${pad(date.getMinutes(), 2)}:${pad(date.getSeconds(), 2)}:` +
+            `${pad(date.getMilliseconds(), 3)}`
+          ) : '';
+        }},
+      ],
+      data: data,
+      datatype: 'local',
+      sortname: 'text',
+      loadonce: true,
+      rowNum: 20,
+      scroll: 1,
+      autowidth: true,
+      forceFit: true,
+      gridview: true,
+      editCell: true,
+    });
+    let width = this.dialog.find('.ui-jqgrid').width();
+    let height = this.grid.parents('.row').height() - this.dialog.find('.ui-jqgrid-hdiv').height();
+    this.grid.jqGrid('setGridHeight', height);
+    this.dialog.on('dialogresize', (e, ui) => {
+      this.grid.jqGrid('setGridWidth', width + (ui.size.width - ui.originalSize.width));
+      this.grid.jqGrid('setGridHeight', height + (ui.size.height - ui.originalSize.height));
+    });
   }
 }
 
--- gfp/utils.js
+++ gfp/utils.js
@@ -3,3 +3,7 @@ export function cache(obj, prop, value){
   Object.defineProperty(obj, prop, {value: value});
   return value;
 }
+
+export function pad(num, size){
+  return Array(Math.max(size - num.toString().length + 1, 0)).join(0) + num;
+}
