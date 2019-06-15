00081d3db6f108a285d3903767f2268cbb0dffa3
Steven Xu <stevenxxiu@gmail.com>
2019-06-15 18:16:35 +1000

    work with google's own nodes that load later too, such as news items

--- gfp/plugin/autopagerize.js
+++ gfp/plugin/autopagerize.js
@@ -4,13 +4,25 @@ export default function(searchGui, _config){
   if(!mainNode || !searchGui)
     return searchGui
   new MutationObserver((mutations) => {
-    // all results have finished loading
+    // all results have finished loading this includes google's own nodes that load later too, such as news items
     let res = []
     for(let mutation of mutations)
       for(let addedNode of mutation.addedNodes)
-        if(addedNode.classList && addedNode.classList.contains('g'))
+        if(addedNode.nodeType != 3)
           res.push(addedNode)
-    searchGui.filterResults({querySelectorAll: (selector) => selector == '.g' ? res : []})
+    if(res.length){
+      // can't wrap parent/grandparent's querySelectorAll, as it will include elements not in addedNodes
+      // can't use node's querySelectorAll, as it doesn't include self
+      // can't create a parent element and move the nodes in it, as the nodes don't get added after the MutationObserver
+      searchGui.filterResults({querySelectorAll: function* (selector){
+        for(let node of res){
+          if(selector == '.g' && node.classList.contains('g'))
+            yield node
+          if(selector == 'g-inner-card' && node.nodeName == 'G-INNER-CARD')
+            yield node
+        }
+      }})
+    }
   }).observe(mainNode, {subtree: true, childList: true})
   return searchGui
 }
