66aa04f5963629f7bf38734143349a2fa846e021
Steven Xu <stevenxxiu@gmail.com>
2021-10-15 14:49:18 +1100

    feat: added `querySelector()` for *AutoPagerize*

--- gfp/plugin/autopagerize.js
+++ gfp/plugin/autopagerize.js
@@ -1,3 +1,31 @@
+function nodeListQuerySelector(nodeList, selector) {
+  // - Can't wrap parent/grandparent's `querySelector()`, as it will include elements not in `addedNodes`.
+  // - Can't use node's `querySelector()`, as it doesn't include the node itself.
+  // - Can't create a parent element and move the nodes in it, as the nodes don't get added after the
+  //   MutationObserver.
+  for (const node of nodeList) {
+    if (node.matches(selector)) {
+      return node
+    }
+    // Call `querySelector()` as well, as `node` can be a results container node
+    const res = node.querySelector(selector)
+    if (res) {
+      return res
+    }
+  }
+}
+
+function nodeListQuerySelectorAll(nodeList, selector) {
+  const matchedNodes = []
+  for (const node of nodeList) {
+    if (node.matches(selector)) {
+      matchedNodes.push(node)
+    }
+    Array.prototype.push.apply(matchedNodes, node.querySelectorAll(selector))
+  }
+  return matchedNodes
+}
+
 function mutationObserver(mutations, searchGui) {
   // All results have finished loading. This includes *Google*'s own nodes that load later too, such as news items.
   const res = []
@@ -10,21 +38,8 @@ function mutationObserver(mutations, searchGui) {
   }
   if (res.length) {
     searchGui.filterResults({
-      querySelectorAll: (selector) => {
-        // - Can't wrap parent/grandparent's `querySelectorAll`, as it will include elements not in `addedNodes`.
-        // - Can't use node's `querySelectorAll`, as it doesn't include the node itself.
-        // - Can't create a parent element and move the nodes in it, as the nodes don't get added after the
-        //   MutationObserver.
-        const matchedNodes = []
-        for (const node of res) {
-          if (node.matches(selector)) {
-            matchedNodes.push(node)
-          }
-          // Call `querySelectorAll` as well, as `node` can be a results container node
-          Array.prototype.push.apply(matchedNodes, node.querySelectorAll(selector))
-        }
-        return matchedNodes
-      },
+      querySelector: (selector) => nodeListQuerySelector(res, selector),
+      querySelectorAll: (selector) => nodeListQuerySelectorAll(res, selector),
     })
   }
 }
